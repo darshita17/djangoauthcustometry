d03f641a0d213c10b9310354598b753b2f0b4028
vagheladj17@gmail.com <darshita.inexture@gmail.com>
2022-10-10 18:45:52 +0530

    add files

---
+++ auth/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import User
+from django.urls import reverse
+
+# Create your views here.
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+    renderer_classes = (UserRenderer,)
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+        return Response(user_data, status=status.HTTP_201_CREATED)
